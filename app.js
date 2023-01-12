c75109fec9067650bdc689df0b0a9dc06b30f458
Seth Lillard <lillardseth@gmail.com>
2023-01-12 17:45:23 -0500

    added form functionality again

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+// navbar
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
-})
+});
+
+// form
+
+let text = document.getElementById('data1');
+let email = document.getElementById('data2');
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', print);
+
+function print (e) {
+    e.preventDefault();
+    console.log(text.value);
+    console.log(email.value);
+    text.value = "";
+    email.value = "";
+}
